6cdc11557739bb2e1962c08fff84698716c98259
zslaykz <zslaykz@gmail.com>
2018-04-12 22:22:41 +0300

    add hero interface

---
+++ src/main/java/hero/Unit.java
@@ -0,0 +1,34 @@
+package hero;
+
+public interface Unit {
+
+    boolean isLeader();
+
+    void setLeader(boolean leader);
+
+    String getName();
+
+    void setName(String name);
+
+    int getCharisma();
+
+    void setCharisma(int charisma);
+
+    int getStamina();
+
+    void setStamina(int stamina);
+
+    int getIntellect();
+
+    void setIntellect(int intellect);
+
+    int getAgility();
+
+    void setAgility(int agility);
+
+    int getConcentration();
+
+    void setConcentration(int concentration);
+
+
+}
